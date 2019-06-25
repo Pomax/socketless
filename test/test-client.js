d2412752e1f88122244202839ca5a7fcd1d0327e
Pomax <pomax@nihongoresources.com>
2019-06-25 15:17:10 -0700

    yay, web

--- test/test-client.js
+++
@@ -1,9 +0,0 @@
-// First, turn the API into a ClientServer object
-const API = require("../demo/API.js");
-const generateClientServer = require("../src/generate-client-server.js");
-const ClientServer = generateClientServer(API);
-
-// Then create a client, using the ClientServer object
-// and a handler class for the client's side of the API:
-const ClientClass = require("../demo/Client.js");
-ClientServer.createClient(`http://localhost:8080`, ClientClass);
